5d8a07e65a49db208c9b72c2875c43c8faaaca22
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2014-01-17 18:44:56 -0200

    Verificação a instalação do pacote do python para manipular o PostgreSQL implementado.

--- src/config.sh
+++ src/config.sh
@@ -6,6 +6,12 @@ if [ "$(dpkg-query -W -f='${Status}' postgresql | grep -c -w "installed")" != "1
 then
 	apt-get install postgresql
 fi
+# Verifica se o Módulo do Python para manipular o PostgreSQL esta instalado.
+if [ "$(dpkg-query -W -f='${Status}' psycopg2 | grep -c -w "installed")" != "1" ]
+then
+	apt-get install python-psycopg2
+fi
+
 
 # TODO 1: descobrir como fazer um usuario e logar via shell-script. Talvez passar login via linha de comando.
 # TODO 2: Após passo 1, listar e verificar se a base de dados ja esta instalada, se não, chamar o arquivo.
